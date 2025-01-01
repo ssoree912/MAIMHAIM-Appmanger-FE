8a7d8c56a42d0067b79c50a3fe456c3b1e0d755f
junsu0306 <76641820+junsu0306@users.noreply.github.com>
2025-01-01 12:11:21 +0900

    wifi 신호 값 변경

--- android/app/src/main/java/com/mynewproject/LocationForegroundService.java
+++ android/app/src/main/java/com/mynewproject/LocationForegroundService.java
@@ -101,9 +101,9 @@ public class LocationForegroundService extends Service {
     private AppDB appDB;  // AppDB 인스턴스 추가
     private String[] packageNames = { "starbucks" , "walmart","costco","ces"};
     private String lastPackageName; // 마지막에 진입한 패키지 이름을 저장
-    private double OUTER_BOUNDARY = 0.25;
+    private double OUTER_BOUNDARY = 0.23;
 
-    private double INNER_BOUNDARY = 0.45;
+    private double INNER_BOUNDARY = 0.43;
     private static LocationForegroundService instance;
     ShakeDetector shakeDetector ;
     public static LocationForegroundService getInstance() {
